2472c39ff0fcaa9091aa0f0a333a602e9e62b467
aidanmc207 <tatanmc2007@gmail.com>
2026-09-16 16:14:01 -0600

    fix: corregidas las fallas por falta de expeciones

---
+++ src/main/java/cr/ac/ucr/paraiso/ie/c4h845/quiz4/business/ClienteNoEncontradoException.java
@@ -0,0 +1,7 @@
+package cr.ac.ucr.paraiso.ie.c4h845.quiz4.business;
+
+public class ClienteNoEncontradoException extends RuntimeException {
+    public ClienteNoEncontradoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/cr/ac/ucr/paraiso/ie/c4h845/quiz4/business/PesoExcedidoException.java
@@ -0,0 +1,7 @@
+package cr.ac.ucr.paraiso.ie.c4h845.quiz4.business;
+
+public class PesoExcedidoException extends RuntimeException {
+    public PesoExcedidoException(String message) {
+        super(message);
+    }
+}
