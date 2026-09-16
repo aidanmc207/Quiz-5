d23276e5f75a7be430dffdb3393988b1fce8c995
aidanmc207 <tatanmc2007@gmail.com>
2026-09-16 16:23:23 -0600

    fix:Errores de dependencias e imports en el PaqueteController

--- src/main/java/cr/ac/ucr/paraiso/ie/c4h845/quiz4/controller/PaqueteController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/c4h845/quiz4/controller/PaqueteController.java
@@ -8,9 +8,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
@@ -60,6 +64,6 @@ public class PaqueteController {
             @ApiResponse(responseCode = "404", description = "Paquete no encontrado")
     })
     public ResponseEntity<String> obtenerPaquete(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body("Endpoint de consulta por id: " + id);
+        return ResponseEntity.ok("Endpoint de consulta por id: " + id);
     }
 }
